9556d0ac263e3bfb55d456a05ccfef64570295f6
Antonio Morga <anotniomc9719@gmail.com>
2022-03-22 11:58:08 +0100

    Created service to fetch data from localStorage

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { UserStore } from '../interfaces/interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  dataKey: string = 'app-user-info';
+  getDataFromLocalStorage() {
+    if (localStorage.getItem('app-user-info')) {
+      return JSON.parse(localStorage.getItem('app-user-info') as string);
+    }
+    return null;
+  }
+
+  saveDataToLocalStorage(data: UserStore) {
+    console.log('app-user-info');
+    if (!localStorage.getItem('app-user-info')) {
+      localStorage.setItem('app-user-info', JSON.stringify(data));
+    } else return this.getDataFromLocalStorage();
+  }
+}
