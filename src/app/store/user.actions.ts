f7deefb7e84eec2ed92c0b81244afa2775c2fdc0
Antonio Morga <anotniomc9719@gmail.com>
2022-03-22 11:59:01 +0100

    Modified reducer and installed ngrx devtools

--- src/app/store/user.actions.ts
+++ src/app/store/user.actions.ts
@@ -1,8 +1,8 @@
 import { createAction, props } from '@ngrx/store';
 import { UserStore } from '../interfaces/interfaces';
 
-export const login = createAction(
-  '[User] Login',
+export const saveUser = createAction(
+  '[User] Save user',
   props<{ userData: UserStore }>()
 );
 export const logout = createAction('[User] Logout');
