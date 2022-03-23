289f82db924f7a6035b187eb9428caf0c8c4a9dd
Antonio Morga <anotniomc9719@gmail.com>
2022-03-23 16:16:24 +0100

    Added test for register component

--- src/app/login-register/register/register.component.spec.ts
+++ src/app/login-register/register/register.component.spec.ts
@@ -1,4 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideMockStore } from '@ngrx/store/testing';
+import { of } from 'rxjs';
 
 import { RegisterComponent } from './register.component';
 
@@ -8,9 +13,10 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      imports: [CommonModule, ReactiveFormsModule, HttpClientTestingModule],
+      providers: [provideMockStore()],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +27,30 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('When calling handleSubmit with valid form values', () => {
+    it('should register the user', () => {
+      spyOn(component.auth, 'registerUser').and.returnValue(
+        of({
+          id: '4f4f4f4f4f4f4f4',
+          teamLeader: 'test',
+          userName: 'test',
+          name: 'test',
+          admin: false,
+          mail: 'test',
+          token: '8k8k8k8k8k8',
+        })
+      );
+
+      component.regForm.controls['userName'].setValue('test');
+      component.regForm.controls['name'].setValue('test');
+      component.regForm.controls['password'].setValue('test');
+      component.regForm.controls['mail'].setValue('test');
+
+      component.handleSubmit();
+
+      expect(component.auth.registerUser).toHaveBeenCalled();
+    });
   });
 });
