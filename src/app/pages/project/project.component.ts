9bceae1d116b521cc2f9702fd51c068e2e20c918
Antonio Morga <anotniomc9719@gmail.com>
2022-03-23 10:56:21 +0100

    Basic implementation of drag and drop

--- src/app/pages/project/project.component.ts
+++ src/app/pages/project/project.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { ifProject, UserStore } from 'src/app/interfaces/interfaces';
+import { ifProject, ifTask, UserStore } from 'src/app/interfaces/interfaces';
 import { ProjectsService } from 'src/app/services/projects.service';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-project',
@@ -13,11 +18,32 @@ export class ProjectComponent implements OnInit {
   id!: string;
   project!: ifProject;
   userData!: UserStore;
+  toDo!: ifTask[];
+  doing!: ifTask[];
+  toReview!: ifTask[];
+  done!: ifTask[];
   constructor(
     private route: ActivatedRoute,
     private projects: ProjectsService,
     private store: Store<{ user: UserStore }>
   ) {}
+
+  drop(event: CdkDragDrop<ifTask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') as string;
@@ -28,7 +54,18 @@ export class ProjectComponent implements OnInit {
       });
     this.projects.getOne(this.userData.token, this.id).subscribe((data) => {
       this.project = data;
-      console.log(data);
+      this.toDo = (this.project.tasks as ifTask[]).filter(
+        (item) => item.status === 'to-do'
+      );
+      this.doing = (this.project.tasks as ifTask[]).filter(
+        (item) => item.status === 'doing'
+      );
+      this.toReview = (this.project.tasks as ifTask[]).filter(
+        (item) => item.status === 'to-review'
+      );
+      this.done = (this.project.tasks as ifTask[]).filter(
+        (item) => item.status === 'done'
+      );
     });
   }
 }
--- src/app/pages/project/project.module.ts
+++ src/app/pages/project/project.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectComponent } from './project.component';
 
-
 @NgModule({
-  declarations: [
-    ProjectComponent
-  ],
-  imports: [
-    CommonModule,
-    ProjectRoutingModule
-  ]
+  declarations: [ProjectComponent],
+  imports: [CommonModule, ProjectRoutingModule, DragDropModule],
 })
-export class ProjectModule { }
+export class ProjectModule {}
