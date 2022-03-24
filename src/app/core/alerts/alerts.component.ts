e313210d758a8a8083d4a6a5a4872dd73487a7fc
Antonio Morga <anotniomc9719@gmail.com>
2022-03-24 10:55:06 +0100

    Created alert component

--- src/app/core/alerts/alerts.component.ts
+++ src/app/core/alerts/alerts.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alerts',
   templateUrl: './alerts.component.html',
-  styleUrls: ['./alerts.component.scss']
+  styleUrls: ['./alerts.component.scss'],
 })
-export class AlertsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AlertsComponent {
+  @Input() isError!: boolean;
+  @Input() message!: string;
+  @Input() active!: boolean;
+  constructor() {}
 }
