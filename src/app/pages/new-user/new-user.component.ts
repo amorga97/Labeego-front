b6dccac3dd309d634fdaeb10ed4d4f81f0fb3839
Antonio Morga <anotniomc9719@gmail.com>
2022-03-25 12:55:02 +0100

    New user component checks if user is admin onInit

--- src/app/pages/new-user/new-user.component.ts
+++ src/app/pages/new-user/new-user.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { UserStore } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.scss']
+  styleUrls: ['./new-user.component.scss'],
 })
 export class NewUserComponent implements OnInit {
-
-  constructor() { }
+  userData!: UserStore;
+  constructor(
+    public store: Store<{ user: UserStore }>,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.store
+      .select((state) => state.user)
+      .subscribe({
+        next: (data) => {
+          this.userData = data;
+          if (!this.userData.admin) {
+            this.router.navigate(['user-dash']);
+          }
+        },
+      });
   }
-
 }
--- src/app/pages/new-user/new-user.module.ts
+++ src/app/pages/new-user/new-user.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { NewUserRoutingModule } from './new-user-routing.module';
 import { NewUserComponent } from './new-user.component';
-
+import { CoreModule } from 'src/app/core/core.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    NewUserComponent
-  ],
+  declarations: [NewUserComponent],
   imports: [
     CommonModule,
-    NewUserRoutingModule
-  ]
+    NewUserRoutingModule,
+    CoreModule,
+    ReactiveFormsModule,
+  ],
 })
-export class NewUserModule { }
+export class NewUserModule {}
