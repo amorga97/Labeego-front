cb1891f91ca6bf97b279e4c11902aac3b93c38ff
Antonio Morga <anotniomc9719@gmail.com>
2022-03-25 10:50:02 +0100

    Added tests for local-storage.service

--- src/app/services/local-storage.service.spec.ts
+++ src/app/services/local-storage.service.spec.ts
@@ -13,4 +13,52 @@ describe('LocalStorageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('When calling service.getDataFromLocalStorage', () => {
+    it('Should call localStorage.getItem', () => {
+      spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify('token'));
+
+      service.getDataFromLocalStorage();
+      expect(localStorage.getItem).toHaveBeenCalled;
+    });
+  });
+  describe('When calling service.getDataFromLocalStorage', () => {
+    it('Should call localStorage.getItem', () => {
+      spyOn(localStorage, 'getItem').and.returnValue(null);
+
+      service.getDataFromLocalStorage();
+      expect(localStorage.getItem).toHaveBeenCalled;
+    });
+  });
+
+  describe('When calling service.saveDataToLocalStorage and there already is a token', () => {
+    it('Should call localStorage.setItem', () => {
+      spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify('token'));
+      spyOn(localStorage, 'setItem');
+
+      service.saveDataToLocalStorage('token');
+      expect(localStorage.getItem).toHaveBeenCalled;
+      expect(localStorage.setItem).not.toHaveBeenCalled;
+    });
+  });
+
+  describe('When calling service.saveDataToLocalStorage and no token is saved yet', () => {
+    it('Should call localStorage.setItem', () => {
+      spyOn(localStorage, 'getItem').and.returnValue(null);
+      spyOn(localStorage, 'setItem');
+
+      service.saveDataToLocalStorage('token');
+      expect(localStorage.getItem).toHaveBeenCalled;
+      expect(localStorage.setItem).toHaveBeenCalled;
+    });
+  });
+
+  describe('When calling service.saveDataToLocalStorage and no token is saved yet', () => {
+    it('Should call localStorage.setItem', () => {
+      spyOn(localStorage, 'removeItem');
+
+      service.clearLocalStorage();
+      expect(localStorage.removeItem).toHaveBeenCalled;
+    });
+  });
 });
