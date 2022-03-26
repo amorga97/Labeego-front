5b5ac5e710d69da40173ea88ab6f826fb2c607e6
Antonio Morga <anotniomc9719@gmail.com>
2022-03-26 15:44:05 +0100

    Generated scaffolding for layout component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { AlertsComponent } from './alerts/alerts.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TeamComponent } from './team/team.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [UserBarComponent, AlertsComponent, SidebarComponent, TeamComponent, ProjectListComponent],
+  declarations: [UserBarComponent, AlertsComponent, SidebarComponent, TeamComponent, ProjectListComponent, LayoutComponent],
   imports: [CommonModule],
   exports: [CommonModule, UserBarComponent, AlertsComponent],
 })
