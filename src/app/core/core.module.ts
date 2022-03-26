d74131aa5e64ba1f1dfa4df636533b3640404486
Antonio Morga <anotniomc9719@gmail.com>
2022-03-26 12:58:32 +0100

    Generated sidebar component scaffolding

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserBarComponent } from './user-bar/user-bar.component';
 import { AlertsComponent } from './alerts/alerts.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [UserBarComponent, AlertsComponent],
+  declarations: [UserBarComponent, AlertsComponent, SidebarComponent],
   imports: [CommonModule],
   exports: [CommonModule, UserBarComponent, AlertsComponent],
 })
