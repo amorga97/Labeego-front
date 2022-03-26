deba755dd2acfae48cab286c47f71d292270dec8
Antonio Morga <anotniomc9719@gmail.com>
2022-03-26 15:39:08 +0100

    Generated project-list component scaffolding

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { UserBarComponent } from './user-bar/user-bar.component';
 import { AlertsComponent } from './alerts/alerts.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TeamComponent } from './team/team.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
-  declarations: [UserBarComponent, AlertsComponent, SidebarComponent, TeamComponent],
+  declarations: [UserBarComponent, AlertsComponent, SidebarComponent, TeamComponent, ProjectListComponent],
   imports: [CommonModule],
   exports: [CommonModule, UserBarComponent, AlertsComponent],
 })
--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  template: `<app-user-bar></app-user-bar> <app-team></app-team>`,
+  template: `<app-user-bar></app-user-bar>
+    <app-project-list></app-project-list> <app-team></app-team>`,
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
