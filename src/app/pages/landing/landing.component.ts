05314963be7ac96544f711daf258e3085e502d03
Antonio Morga <anotniomc9719@gmail.com>
2022-03-29 21:52:35 +0200

    Updated test for user-bar component

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,49 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
   linePath: any;
   linePathLength: any;
   mazePath: any;
   mazePathLength: any;
 
-  constructor() {
-    window.addEventListener('DOMContentLoaded', () => {
-      this.linePath = document.querySelector('.line');
-      this.linePathLength = this.linePath?.getTotalLength();
-      this.linePath.style.strokeDasharray =
-        this.linePathLength + ' ' + this.linePathLength;
-      this.linePath.style.strokeDashoffset = this.linePathLength;
-
-      this.mazePath = document.querySelector('.maze');
-      this.mazePathLength = this.mazePath?.getTotalLength();
-      this.mazePath.style.strokeDasharray =
-        this.mazePathLength + ' ' + this.mazePathLength;
-      this.mazePath.style.strokeDashoffset = this.mazePathLength;
-    });
-
-    window.addEventListener('scroll', () => {
-      let scrollPercentage =
-        document.documentElement.scrollTop /
-        (document.documentElement.scrollHeight -
-          document.documentElement.clientHeight);
-
-      let drowLength = this.linePathLength * scrollPercentage;
-
-      this.linePath.style.strokeDashoffset = this.linePathLength - drowLength;
-
-      let mazeDrowLength = this.mazePathLength * (scrollPercentage * 1.7);
-
-      this.mazePath.style.strokeDashoffset =
-        this.mazePathLength - mazeDrowLength;
-    });
+  constructor(@Inject(DOCUMENT) public document: Document) {
+    window.addEventListener('DOMContentLoaded', this.queryDocument);
+    window.addEventListener('scroll', this.configureScrollEffect);
   }
 
-  ngOnInit(): void {
-    'nothing';
+  queryDocument() {
+    this.linePath = this.document.querySelector('.line');
+    this.linePathLength = this.linePath?.getTotalLength();
+    this.linePath.style.strokeDasharray =
+      this.linePathLength + ' ' + this.linePathLength;
+    this.linePath.style.strokeDashoffset = this.linePathLength;
+
+    this.mazePath = document.querySelector('.maze');
+    this.mazePathLength = this.mazePath?.getTotalLength();
+    this.mazePath.style.strokeDasharray =
+      this.mazePathLength + ' ' + this.mazePathLength;
+    this.mazePath.style.strokeDashoffset = this.mazePathLength;
+  }
+
+  configureScrollEffect() {
+    let scrollPercentage =
+      this.document.documentElement.scrollTop /
+      (this.document.documentElement.scrollHeight -
+        this.document.documentElement.clientHeight);
+
+    let drowLength = this.linePathLength * scrollPercentage;
+
+    this.linePath.style.strokeDashoffset = this.linePathLength - drowLength;
+
+    let mazeDrowLength = this.mazePathLength * (scrollPercentage * 1.7);
+
+    this.mazePath.style.strokeDashoffset = this.mazePathLength - mazeDrowLength;
   }
 }
